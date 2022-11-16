be9ed65d15fcbd93caf7c7c88c9c08779869a903
arik-dig <asher@digma.ai>
2022-11-16 11:58:47 +0200

    bring back context propagator

--- grpc/src/main/java/io/opentelemetry/example/grpc/ExampleConfiguration.java
+++ grpc/src/main/java/io/opentelemetry/example/grpc/ExampleConfiguration.java
@@ -8,6 +8,8 @@ package io.opentelemetry.example.grpc;
 import static io.opentelemetry.semconv.resource.attributes.ResourceAttributes.SERVICE_NAME;
 
 import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
+import io.opentelemetry.context.propagation.ContextPropagators;
 import io.opentelemetry.exporter.logging.LoggingSpanExporter;
 import io.opentelemetry.exporter.otlp.metrics.OtlpGrpcMetricExporter;
 import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
@@ -39,6 +41,8 @@ public final class ExampleConfiguration {
 
     OpenTelemetrySdk openTelemetrySdk =
         OpenTelemetrySdk.builder()
+            // install the W3C Trace Context propagator
+            .setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
             .setTracerProvider(
                 SdkTracerProvider.builder()
                     .setResource(resource)
